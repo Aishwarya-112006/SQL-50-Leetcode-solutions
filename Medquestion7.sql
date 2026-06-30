6cc97baa94c391ac1086287665a706daf77b28c8
Aishwarya <aishwarya.singh.cs28@iilm.edu>
2026-06-30 22:46:13 +0530

    Create Medquestion7.sql

---
+++ Medquestion7.sql
@@ -0,0 +1,32 @@
+/*Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+*/
+select name from employee where id in (
+    select managerID from employee group by managerId 
+    having count(*)>=5
+);
